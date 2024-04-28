3eab759db12b58152d2b43bbf2f02cd5bfda3d21
Arnau Diaz <diazarnau0@gmail.com>
2024-04-28 18:15:32 +0200

    format

--- src/views/PlanView.tsx
+++ src/views/PlanView.tsx
@@ -7,12 +7,12 @@ export default function PlanView({}: PlanViewProps) {
 	<div className="grid grid-cols-3 gap-4">
 		<Cluster />
 		<div className="grid grid-cols-subgrid grid-cols-2 gap-4">
-		<Component exercise='Bench press' kgs='85' increase={24} />
-		<Component exercise='Back squat' kgs='115' increase={8} />
+			<Component exercise='Bench press' kgs='85' increase={24} />
+			<Component exercise='Back squat' kgs='115' increase={8} />
 		</div>
 		<div className="grid grid-cols-subgrid grid-cols-2 gap-4">
-		<Component exercise='Weighted pullups' kgs='15' increase={14} />
-		<Component exercise='Total volume' kgs='321' increase={32} />
+			<Component exercise='Weighted pullups' kgs='15' increase={14} />
+			<Component exercise='Total volume' kgs='321' increase={32} />
 		</div>
 	</div>
 	);
