400b8f2169ae78fcd15b90133502d670da319150
Arnau Diaz <diazarnau0@gmail.com>
2024-05-01 13:18:21 +0200

    plan view for all exercises type available

--- src/views/PlanView.tsx
+++ src/views/PlanView.tsx
@@ -7,15 +7,10 @@ export default function PlanView() {
 
 	const getIncrease = (ex: ExerciseType) => Math.round(((getWeight(userConfig, ex)) - getWeightLast(userConfig, ex)) / getWeightLast(userConfig, ex) * 100);
 
-	const exercises = [
-		{ name: toName(ExerciseType.BENCH_PRESS), kgs: getWeight(userConfig, ExerciseType.BENCH_PRESS).toString(), increase: getIncrease(ExerciseType.BENCH_PRESS) },
-		{ name: toName(ExerciseType.BACK_SQUAT), kgs: getWeight(userConfig, ExerciseType.BACK_SQUAT).toString(), increase: getIncrease(ExerciseType.BACK_SQUAT) },
-		{ name: toName(ExerciseType.WEIGHTED_PULLUPS), kgs: getWeight(userConfig, ExerciseType.WEIGHTED_PULLUPS).toString(), increase: getIncrease(ExerciseType.WEIGHTED_PULLUPS) },
-		{ name: toName(ExerciseType.DL), kgs: getWeight(userConfig, ExerciseType.DL).toString(), increase: getIncrease(ExerciseType.DL) },
-		{ name: toName(ExerciseType.OHP), kgs: getWeight(userConfig, ExerciseType.OHP).toString(), increase: getIncrease(ExerciseType.OHP) },
-		// TODO total
-		// { name: 'Total volume', kgs: total.toString(), increase: totalIncrease }
-	];
+	const exercises: { name: string, kgs: string, increase: number }[] = [ExerciseType.BENCH_PRESS, ExerciseType.BACK_SQUAT, ExerciseType.WEIGHTED_PULLUPS, ExerciseType.DL, ExerciseType.OHP]
+		.map(ex => {
+			return { name: toName(ex), kgs: getWeight(userConfig, ex).toString(), increase: getIncrease(ex) }
+		});
 
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
