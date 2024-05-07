d33a8e02c3161e8b6718bce2b862c626cb902f0f
Arnau Diaz <diazarnau0@gmail.com>
2024-05-07 11:12:51 +0200

    benchmark view added but dissabled

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { Separator } from "@/components/ui/separator"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 
-import PlanView from './views/PlanView'
 import { UserConfigProvider } from './contexts/UserConfigContext'
 import ProgramOperatorView from './views/ProgramOperatorView'
 import Program531BView from './views/Program531BView'
 import ProgramGZCLPView from './views/ProgramGZCLPView'
+import ProgressView from './views/ProgressView'
 
 
 function App() {
@@ -21,15 +21,15 @@ function App() {
 
       <UserConfigProvider>
         <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/80 p-4 md:gap-8 md:p-10">
-          <Tabs defaultValue="plan" className="mb-4">
+          <Tabs defaultValue="progress" className="mb-4">
             <TabsList>
-              <TabsTrigger value="plan">Plan</TabsTrigger>
+              <TabsTrigger value="progress">Progress</TabsTrigger>
               <TabsTrigger value="program-operator">Operator</TabsTrigger>
               <TabsTrigger value="program-531b">5/3/1 Beginner</TabsTrigger>
               <TabsTrigger value="program-gzclp">GZCLP</TabsTrigger>
             </TabsList>
-            <TabsContent value="plan">
-              <PlanView />
+            <TabsContent value="progress">
+              <ProgressView />
             </TabsContent>
             <TabsContent value="program-operator">
               <ProgramOperatorView />
--- src/views/ProgressView.tsx
+++ src/views/ProgressView.tsx
@@ -1,8 +1,9 @@
+import Benchmark from "@/components/Benchmark";
 import Cluster from "@/components/Cluster";
 import Component from "@/components/Component";
 import { ExerciseType, UserConfig, getWeight, getWeightLast, toName, useUserConfig } from "@/contexts/UserConfigContext";
 
-export default function PlanView() {
+export default function ProgressView() {
 	const userConfig: UserConfig = useUserConfig();
 
 	const getIncrease = (ex: ExerciseType) => Math.round(((getWeight(userConfig, ex)) - getWeightLast(userConfig, ex)) / getWeightLast(userConfig, ex) * 100);
@@ -12,7 +13,7 @@ export default function PlanView() {
 			return { name: toName(ex), kgs: getWeight(userConfig, ex).toString(), increase: getIncrease(ex) }
 		});
 
-	return (
+	return (<>
 		<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 			<Cluster />
 			<div className="grid grid-cols-1 md:grid-cols-3 md:auto-cols-max md:auto-rows-max gap-4">
@@ -26,5 +27,9 @@ export default function PlanView() {
 				))}
 			</div>
 		</div>
-	);
+		<div className="grid grid-cols-1">
+			{/* TODO */}
+			{/* <Benchmark /> */}
+		</div>
+	</>);
 }
